a403292138f3a2f78201c1e70e4f0dde72e108d1
aliozgurarslan <117865292+aliozgurarslan@users.noreply.github.com>
2024-07-27 23:39:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ new Vue({
         correctItems: [],
         selectedItems: [],
         previousGuesses: [],
-        correctGuessesOrder: [],
         attemptsLeft: 5,
         wrongGuessMessage: "",
         isWrong: false,
@@ -37,12 +36,14 @@ new Vue({
         },
         correctGroupsWithMessages() {
             let groupsWithMessages = [];
-            for (let i = 0; i < this.correctGuessesOrder.length; i++) {
-                let groupIndex = this.correctGuessesOrder[i];
-                groupsWithMessages.push({
-                    items: this.correctGroups[groupIndex],
-                    message: this.correctGroupMessages[groupIndex]
-                });
+            for (let i = 0; i < this.correctGroups.length; i++) {
+                let groupItems = this.correctGroups[i];
+                if (groupItems.every(item => this.correctItems.includes(item))) {
+                    groupsWithMessages.push({
+                        items: groupItems,
+                        message: this.correctGroupMessages[i]
+                    });
+                }
             }
             return groupsWithMessages;
         }
@@ -72,14 +73,9 @@ new Vue({
 
             this.previousGuesses.push(currentGuess);
 
-            let isCorrect = false;
-            for (let i = 0; i < this.correctGroups.length; i++) {
-                if (this.arraysEqual(this.correctGroups[i].sort(), this.selectedItems.sort())) {
-                    isCorrect = true;
-                    this.correctGuessesOrder.push(i);
-                    break;
-                }
-            }
+            let isCorrect = this.correctGroups.some(group => {
+                return this.arraysEqual(group.sort(), this.selectedItems.sort());
+            });
 
             if (isCorrect) {
                 this.correctItems.push(...this.selectedItems);
@@ -94,8 +90,8 @@ new Vue({
                 }, 3000);
                 this.attemptsLeft--;
                 if (this.attemptsLeft === 0) {
-                    this.revealAllGroups();
                     this.wrongGuessMessage = 'Tüm denemeler bitti. Oyun bitti!';
+                    this.revealAnswers();
                 }
             }
 
@@ -111,12 +107,11 @@ new Vue({
         shuffleItems() {
             this.items = this.items.sort(() => Math.random() - 0.5);
         },
-        revealAllGroups() {
-            for (let i = 0; i < this.correctGroups.length; i++) {
-                if (!this.correctGuessesOrder.includes(i)) {
-                    this.correctGuessesOrder.push(i);
-                }
-            }
+        deselectAll() {
+            this.selectedItems = [];
+        },
+        revealAnswers() {
+            this.correctItems = [...this.items];
         }
     }
 });
